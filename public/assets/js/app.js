f1dfe0313edc9be73b574d744bab61e9510dad88
Ryan Kooy <48595828+ryankooy@users.noreply.github.com>
2019-11-03 16:00:01 -0500

    Update app.js

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,36 +1,23 @@
 $('#view').on('click', () => {
-  $('#note').empty();
   let thisId = $(this).attr('data-id');
 
-  $.ajax({
-    method: 'GET',
-    url: '/articles/' + thisId
-  })
-  .then(data => {
-    console.log(data);
-    data.render({
-      title: data.title,
-      body: data.body
-    });
-  });
+  $.get('/articles/' + thisId, (req, res) => {
+     console.log(res);
+     res.render({ note: res });
+   });
 });
 
 $('#save').on('click', () => {
   let thisId = $(this).attr('data-id');
-  
-  const newNote = new Note(req.body);
-  newNote.save();
 
-  $.ajax({
-    method: 'POST',
-    url: '/articles/' + thisId,
-    data: {
-      title: $('#save-title').val(),
-      body: $('#save-note').val()
-    }
-  })
-  .then(data => {
-    console.log(data);
+  const newNote = new Note({
+    title: $('#save-title').val(),
+    body: $('#save-note').val()
+  });
+  newNote.save();
+  
+  $.post('/articles/' + thisId, newNote, (req, res) => {
+    console.log(res);
     $('#note').empty();
   });
 
